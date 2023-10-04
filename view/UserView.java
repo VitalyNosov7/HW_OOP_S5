40b52c5ff7abaf4c8012480c34de8c27f34494ca
Vitaly <test@example.com>
2023-10-04 16:16:30 +0300

    Initial commit

---
+++ view/UserView.java
@@ -0,0 +1,77 @@
+package notebook.view;
+
+import notebook.controller.UserController;
+import notebook.model.User;
+import notebook.util.Commands;
+
+import java.util.Scanner;
+
+public class UserView {
+    private final UserController userController;
+
+    public UserView(UserController userController) {
+        this.userController = userController;
+    }
+
+    public void run() {
+        Commands com;
+
+        while (true) {
+            String command = prompt("Введите команду: ");
+            com = Commands.valueOf(command);
+            if (com == Commands.EXIT) return;
+            switch (com) {
+                case CREATE:
+                    User u = createUser();
+                    userController.saveUser(u);
+                    break;
+                case READ:
+                    String id = prompt("Идентификатор пользователя: ");
+                    try {
+                        User user = userController.readUser(Long.parseLong(id));
+                        System.out.println(user);
+                        System.out.println();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case UPDATE:
+                    String userId = prompt("Enter user id: ");
+                    userController.updateUser(userId, createUser());
+                case DELETE:
+                    String delete = prompt("Идентификатор пользователя: ");
+                    if (delete.isEmpty()) {
+                        throw new RuntimeException("Идентификатор не может быть пустым");
+                    } else {
+                        userController.deleteUser(delete);
+                    }
+                case LIST:
+                    System.out.println(userController.getAllUsers());
+
+            }
+        }
+    }
+
+    private String prompt(String message) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        return in.nextLine();
+    }
+
+    private User createUser() {
+        String firstName = prompt("Имя: ");
+        if (firstName.isEmpty()) {
+            throw new RuntimeException("Имя не может быть пустым");
+        }
+        String lastName = prompt("Фамилия: ");
+        if (lastName.isEmpty()) {
+            throw new RuntimeException("Фамилия не может быть пустой");
+        }
+        String phone = prompt("Номер телефона: ");
+        if (phone.isEmpty()) {
+            throw new RuntimeException("Телефон не может быть пустым");
+        }
+
+        return new User(firstName.replaceAll(" ", ""), lastName.replaceAll(" ", ""), phone);
+    }
+}
